3331ddb770b647a7bdf0efcec6e651bdc4429e08
henrydjpacheco <henrydjpacheco@gmailcom>
2023-07-18 10:49:10 -0500

    ultimate changes now yesss

--- api/src/db.js
+++ api/src/db.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 const { Sequelize } = require('sequelize');
 const fs = require('fs');
 const path = require('path');
-const { DB_USER, DB_PASSWORD, DB_HOST, DB_PORT, DB_NAME } = process.env;
+const { DB_USER, DB_PASSWORD, DB_HOST, DB_PORT, DB_NAME, DB_URL } = process.env;
 const sequelize = new Sequelize(
   /*  `postgresql://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_NAME}` */`${DB_URL}`,
    {
